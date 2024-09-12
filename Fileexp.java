af75fb89d64743094265a2db514c0032011a1b6e
Aradhana0112 <159871735+Aradhana0112@users.noreply.github.com>
2024-09-11 22:12:03 -0700

    Add files via upload

---
+++ Fileexp.java
@@ -0,0 +1,30 @@
+import java.io.*;
+class Fileexp
+{
+  public static void main(String []args)
+  {
+    File f = new File("D:\\java\\exp.txt");
+  
+ try{
+     if(f.createNewFile()){
+     System.out.println("file is created");
+    }
+     else{
+     System.out.println("File is exist");
+     }
+
+     FileReader fr = new FileReader(f);
+     BufferedReader br = new BufferedReader(fr);
+     String line;
+       while ((line = br.readLine()) != null) {
+            System.out.println(line);
+       }
+       br.close();
+       fr.close();
+   }
+      
+      catch(IOException e){
+               System.out.println("ERROR "+e.getMessage());
+      }
+}
+}
